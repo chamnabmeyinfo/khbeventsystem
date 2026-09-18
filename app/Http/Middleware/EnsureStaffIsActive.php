67d25c5373d8dbd552afc3e58dec058c76142736
chamnab mey <chamnabmey.info@gmail.com>
2026-09-18 12:40:07 +0700

    Fix: Correct namespace and import slashes in EnsureStaffIsActive middleware

--- app/Http/Middleware/EnsureStaffIsActive.php
+++ app/Http/Middleware/EnsureStaffIsActive.php
@@ -1,11 +1,11 @@
 <?php
 
-namespace AppHttpMiddleware;
+namespace App\Http\Middleware;
 
 use Closure;
-use IlluminateHttpRequest;
-use IlluminateSupportFacadesAuth;
-use SymfonyComponentHttpFoundationResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Symfony\Component\HttpFoundation\Response;
 
 class EnsureStaffIsActive
 {
